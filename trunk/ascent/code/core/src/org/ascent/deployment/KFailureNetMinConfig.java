8c57c8a5484281f9e8219ec24d512e0842a4f5b0
thompchr <thompchr@1c6ca956-0256-0410-ac26-2fd0b460a87d>
2009-02-24 03:19:27 +0000

    Created KFailureNetMinConfig class

---
+++ trunk/ascent/code/core/src/org/ascent/deployment/KFailureNetMinConfig.java
@@ -0,0 +1,50 @@
+ /**************************************************************************
+ * Copyright 2009 Jules White                                              *
+ *                                                                         *
+ * Licensed under the Apache License, Version 2.0 (the "License");         *
+ * you may not use this file except in compliance with the License.        *
+ * You may obtain a copy of the License at                                 *
+ *                                                                         *
+ * http://www.apache.org/licenses/LICENSE-2.0                              *
+ *                                                                         *
+ * Unless required by applicable law or agreed to in writing, software     *
+ * distributed under the License is distributed on an "AS IS" BASIS,       *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.*
+ * See the License for the specific language governing permissions and     *
+ * limitations under the License.                                          *
+ **************************************************************************/
+
+
+package org.ascent.deployment;
+
+import java.util.ArrayList;
+
+public class KFailureNetMinConfig extends NetMinConfig {
+
+	public KFailureNetMinConfig(Node[] nodes, NetworkLink[] networks,
+			Component[] components, Interaction[] interactions, int failures) {
+		super(nodes, networks, components, interactions);
+		
+		//Make the components array big enough
+		if (failures != 0){
+			
+			ArrayList<Component> temp = new ArrayList<Component>();
+			//Iterate through the components and make duplicates
+			for(Component c:components){
+				temp.add(c);
+				for (int i = 0; i < failures; ++i){
+					temp.add(c);
+				}
+			}
+			
+			
+			
+		}
+		
+		
+		
+		
+		
+
+	}
+}
