d1be6f4e98be97f0612893a600267ff659e4d8a5
shcampbell7 <shcampbell7@1c6ca956-0256-0410-ac26-2fd0b460a87d>
2009-03-23 15:51:57 +0000

    Added Naomi Attribute support for simple attributes

--- ascent/code/excel/src/org/jnexcel/src/org/ascent/excel/naomi_excel_xls.java
+++ ascent/code/excel/src/org/jnexcel/src/org/ascent/excel/naomi_excel_xls.java
@@ -10,15 +10,18 @@ public class naomi_excel_xls {
 
 		// Change this to read in the filename from the command line
 		// so it doesn't have to be hard-coded.
-		String in_file = "test.xls";
-		String in_names = "";				//just a place holder for now
-		String out_file = "output.xls";
-		String out_names = "";				//just a place holder for now
+		String file_prefix = "";	// Change this to be whatever is necessary on your machine.
+		String in_file = file_prefix + "\\test.xls";
+		//String in_names = "";				//just a place holder for now
+		String out_file = file_prefix + "\\output.xls";
+		//String out_names = "";				//just a place holder for now
+		String utils_loc = file_prefix + "\\Artifacts";
 		
 		ExConnector ex = new ExConnector();
-		ex.readInput(in_file, in_names);
+		ex.readInput(in_file);
+		ex.readAttributes(utils_loc);
 		ex.execute();
-		ex.writeOutput(out_file, out_names);
+		ex.writeOutput(out_file);
 		
 	}
 
